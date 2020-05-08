370093484e20ac14e32a8e1628fbec32a80da3b3
Aleksandr Ohotnikov <aleksohotnikov@gmail.com>
2020-05-08 11:30:16 +0400

    Initial Commit

---
+++ clean-up-contacts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  clean-up-contacts
+//
+//  Created by Саша on 08.05.2020.
+//  Copyright © 2020 Саша. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
