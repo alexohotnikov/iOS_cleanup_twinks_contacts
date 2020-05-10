a03438643e0c9a18a1db29f47eff71c6ca5cf08a
Aleksandr Ohotnikov <aleksohotnikov@gmail.com>
2020-05-10 12:15:45 +0400

    [~]:    Function at indep.    Added button OK in final scene. Now you can go back to first scene.

--- clean-up-contacts/ContentView.swift
+++ clean-up-contacts/ContentView.swift
@@ -23,35 +23,24 @@ struct ContentView: View {
                 Spacer()
                            .frame(height: 20)
                 Text("Was Deleted by us: \(wasDeleted) dub constacts.")
+                Spacer()
+                    .frame(height: 20)
+                Button(action: {
+                    self.isDone = false
+                }){
+                    Text("OK")
+                    .bold()
+                        .font(.system(size: 32))
+                }
             } else {
                 Text("Prepare for clean-up dublicate contacts:")
                         .font(.headline)
                     Spacer()
                         .frame(height: 20)
-                    Button(action: {
-                        let contactStore = CNContactStore()
-                        let keys = [CNContactPhoneNumbersKey, CNContactFamilyNameKey, CNContactGivenNameKey, CNContactNicknameKey, CNContactPhoneNumbersKey]
-                        let request1 = CNContactFetchRequest(keysToFetch: keys  as [CNKeyDescriptor])
-                        
-                        var watcher = [String]()
-
-                        try? contactStore.enumerateContacts(with: request1) { (contact, error) in
-                            for phone in contact.phoneNumbers {
-                                let data = watcher.firstIndex(where: { $0.elementsEqual(phone.value.stringValue)})
-                                
-                                if (data != nil) {
-                                    let request = CNSaveRequest()
-                                    let mutable = contact.mutableCopy() as! CNMutableContact
-                                    request.delete(mutable)
-                                    self.wasDeleted += 1
-                                    try? contactStore.execute(request)
-                                } else {
-                                         watcher.append(phone.value.stringValue)
-                                }
-                                self.isDone = true
-                            }
-                        }
-                    }) {
+                Button(action: {
+                    self.wasDeleted = contactMagic()
+                    self.isDone = true
+                }) {
                         Text("Start NOW!")
                             .bold()
                     }
@@ -67,3 +56,31 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+
+
+func contactMagic() -> Int16 {
+    var wasDeleted = 0
+    let contactStore = CNContactStore()
+    let keys = [CNContactPhoneNumbersKey, CNContactFamilyNameKey, CNContactGivenNameKey, CNContactNicknameKey, CNContactPhoneNumbersKey]
+    let request1 = CNContactFetchRequest(keysToFetch: keys  as [CNKeyDescriptor])
+    
+    var watcher = [String]()
+
+    try? contactStore.enumerateContacts(with: request1) { (contact, error) in
+        for phone in contact.phoneNumbers {
+            let data = watcher.firstIndex(where: { $0.elementsEqual(phone.value.stringValue)})
+            
+            if (data != nil) {
+                let request = CNSaveRequest()
+                let mutable = contact.mutableCopy() as! CNMutableContact
+                request.delete(mutable)
+                wasDeleted += 1
+                try? contactStore.execute(request)
+            } else {
+                     watcher.append(phone.value.stringValue)
+            }
+        }
+    }
+    
+    return Int16(wasDeleted);
+}
